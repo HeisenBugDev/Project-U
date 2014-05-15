f238bac3df4ad7be0882b0739f37e784848e3dc2
AzureusNation <alphatango369@gmail.com>
2014-05-15 20:00:25 +0100

    changed resource location from string to ResourceLocation

--- src/main/java/com/heisenbugdev/heisenui/view/element/HeisenTextureView.java
+++ src/main/java/com/heisenbugdev/heisenui/view/element/HeisenTextureView.java
@@ -1,10 +1,11 @@
 package com.heisenbugdev.heisenui.view.element;
 
 import com.heisenbugdev.heisenui.view.HeisenView;
+import net.minecraft.util.ResourceLocation;
 
 public class HeisenTextureView extends HeisenView
 {
 
-    private String textureLocation;
+    private ResourceLocation textureLocation;
 
 }
