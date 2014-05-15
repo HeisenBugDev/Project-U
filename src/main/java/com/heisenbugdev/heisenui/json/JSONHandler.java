507467918dc2df60982bb497d539bc67e19796c0
AzureusNation <alphatango369@gmail.com>
2014-05-15 15:52:43 +0100

    JSON Mapping

--- src/main/java/com/heisenbugdev/heisenui/json/JSONHandler.java
+++ src/main/java/com/heisenbugdev/heisenui/json/JSONHandler.java
@@ -10,19 +10,23 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class JSONHandler {
 
-    public static void jsonLoader(String fileLocation) throws IOException {
+    public static Map jsonLoader(String fileLocation) throws IOException {
 
         Gson gson = new Gson();
+
+        Map data = null;
 
         try {
             if(HeisenUI.DEBUG) HeisenLogger.debug("Reading JSON file" + fileLocation);
 
             BufferedReader br = new BufferedReader(new FileReader(fileLocation));
 
-            JSONGUI jsonGui = gson.fromJson(br, JSONGUI.class);
+            data = gson.fromJson(br, Map.class);
+
         } catch (FileNotFoundException e) {
             HeisenLogger.warn(e + fileLocation);
         } catch (JsonSyntaxException e) {
@@ -31,10 +35,7 @@ public class JSONHandler {
             HeisenLogger.warn(e);
         }
 
-
-        /*Reader reader = new InputStreamReader(JSONHandler.class.getResourceAsStream(fileLocation), "UTF-8");
-        Gson gson = new GsonBuilder().create();
-        JSONGUI gui = gson.fromJson(reader, JSONGUI.class);*/
+        return data;
 
     }
 
