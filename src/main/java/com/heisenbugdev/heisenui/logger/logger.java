e1f1bcb3d53c9e971e253986d6949307cb57eca9
AzureusNation <alphatango369@gmail.com>
2014-05-15 01:25:56 +0100

    fixed logger

--- src/main/java/com/heisenbugdev/heisenui/logger/logger.java
+++ src/main/java/com/heisenbugdev/heisenui/logger/logger.java
@@ -1,8 +1,8 @@
 package com.heisenbugdev.heisenui.logger;
 
+import com.heisenbugdev.heisenui.HeisenProperties;
 import cpw.mods.fml.common.FMLLog;
-
-import java.util.logging.Level;
+import org.apache.logging.log4j.Level;
 
 /**
  * Created by Ash on 15/05/2014.
@@ -10,7 +10,7 @@ import java.util.logging.Level;
 public class logger {
 
     public static void log(Level logLevel, Object object) {
-        FMLLog.log("HeisenUI", logLevel, "[HeisenUI] " + String.valueOf(object));
+        FMLLog.log(HeisenProperties.MOD_IDENTIFICATION, logLevel, "[HeisenUI] " + String.valueOf(object));
     }
 
     public static void off(Object object) {
@@ -21,28 +21,20 @@ public class logger {
         log(Level.INFO, object);
     }
 
-    public static void config(Object object) {
-        log(Level.CONFIG, object);
+    public static void debug(Object object) {
+        log(Level.DEBUG, object);
     }
 
-    public static void warning(Object object) {
-        log(Level.WARNING, object);
+    public static void warn(Object object) {
+        log(Level.WARN, object);
     }
 
-    public static void severe(Object object) {
-        log(Level.SEVERE, object);
+    public static void fatal(Object object) {
+        log(Level.FATAL, object);
     }
 
-    public static void fine(Object object) {
-        log(Level.FINE, object);
-    }
-
-    public static void finer(Object object) {
-        log(Level.FINER, object);
-    }
-
-    public static void finest(Object object) {
-        log(Level.FINEST, object);
+    public static void trace(Object object) {
+        log(Level.TRACE, object);
     }
 
     public static void all(Object object) {
