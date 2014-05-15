30b91873cbe8687f9e50a1c02b3589ab5ed53f3a
Jake Rhoda <jake@jadarstudios.com>
2014-05-15 13:02:39 -0500

    reformat all the code

--- src/main/java/com/heisenbugdev/heisenui/view/element/HeisenButton.java
+++ src/main/java/com/heisenbugdev/heisenui/view/element/HeisenButton.java
@@ -2,7 +2,8 @@ package com.heisenbugdev.heisenui.view.element;
 
 import com.heisenbugdev.heisenui.view.HeisenView;
 
-public class HeisenButton extends HeisenView {
+public class HeisenButton extends HeisenView
+{
 
     private String text;
 
