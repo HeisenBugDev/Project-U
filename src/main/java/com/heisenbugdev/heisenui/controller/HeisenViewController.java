4a49c7805b1f9dbcba12322ee88cf75b73ad4cd0
Jake Rhoda <jake@jadarstudios.com>
2014-05-14 19:59:44 -0500

    basic view controller

---
+++ src/main/java/com/heisenbugdev/heisenui/controller/HeisenViewController.java
@@ -0,0 +1,31 @@
+package com.heisenbugdev.heisenui.controller;
+
+import com.heisenbugdev.heisenui.HeisenUI;
+
+public class HeisenViewController
+{
+
+    public HeisenViewController parentViewController;
+    public HeisenUI view;
+
+
+    public HeisenViewController()
+    {
+
+    }
+
+    private void connectOutlets()
+    {
+
+    }
+
+    private void connectTargets()
+    {
+
+    }
+
+    public void guiWillAppear() {}
+    public void guiDidAppear() {}
+    public void guiWillDisappear() {}
+    public void guiDidDisappear() {}
+}
