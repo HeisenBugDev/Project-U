3d852bae9834866e05be808d2fa0a41fd86a5859
Jake Rhoda <jake@jadarstudios.com>
2014-05-15 09:54:51 -0500

    made some changes to json handler for deserializing JSON

--- src/main/java/com/heisenbugdev/heisenui/json/JSONHandler.java
+++ src/main/java/com/heisenbugdev/heisenui/json/JSONHandler.java
@@ -5,37 +5,59 @@ import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import com.heisenbugdev.heisenui.HeisenUI;
 import com.heisenbugdev.heisenui.logger.HeisenLogger;
+import net.minecraft.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class JSONHandler {
 
-    public static void jsonLoader(String fileLocation) throws IOException {
+    public static Map jsonLoaderFromFileLocation(String fileLocation) throws IOException
+    {
+        if(HeisenUI.DEBUG) HeisenLogger.debug("Reading JSON file" + fileLocation);
+        String json = "";
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(fileLocation));
+            json = "";
+            while (br.ready())
+            {
+                json += br.readLine();
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            HeisenLogger.warn(e + fileLocation);
+        }
+
+        if (StringUtils.isNullOrEmpty(json)) return null;
+
+        return jsonLoader(json);
+    }
+
+    public static Map jsonLoader(String json) throws IOException {
 
         Gson gson = new Gson();
 
-        try {
-            if(HeisenUI.DEBUG) HeisenLogger.debug("Reading JSON file" + fileLocation);
+        Map data = null;
 
-            BufferedReader br = new BufferedReader(new FileReader(fileLocation));
-
-            JSONGUI jsonGui = gson.fromJson(br, JSONGUI.class);
-        } catch (FileNotFoundException e) {
-            HeisenLogger.warn(e + fileLocation);
-        } catch (JsonSyntaxException e) {
+        try
+        {
+            data = gson.fromJson(json, Map.class);
+        }
+        catch (JsonSyntaxException e)
+        {
             HeisenLogger.warn(e);
-        } catch (JsonIOException e) {
+        }
+        catch (JsonIOException e)
+        {
             HeisenLogger.warn(e);
         }
 
-
-        /*Reader reader = new InputStreamReader(JSONHandler.class.getResourceAsStream(fileLocation), "UTF-8");
-        Gson gson = new GsonBuilder().create();
-        JSONGUI gui = gson.fromJson(reader, JSONGUI.class);*/
-
+        return data;
     }
 
 }
