d0626c53159411b4102b9d19a49fc649cb975251
AzureusNation <alphatango369@gmail.com>
2014-05-15 18:28:04 +0100

    Added Debugging item

---
+++ src/main/java/com/heisenbugdev/heisenui/DebugItem.java
@@ -0,0 +1,24 @@
+package com.heisenbugdev.heisenui;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class DebugItem extends Item {
+
+    public DebugItem() {
+        this.setUnlocalizedName("debugItem");
+        this.setMaxStackSize(1);
+
+        this.setCreativeTab(CreativeTabs.tabAllSearch);
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+        //player.openGui(HeisenProperties.MOD_IDENTIFICATION, GUIID, player.worldObj, (int)player.posX, (int)player.posY, (int)player.posZ);
+        return itemStack;
+    }
+
+}
