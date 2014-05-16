04919e9c043a47092095c5c7ed06c2490b67c22a
Jake Rhoda <jake@jadarstudios.com>
2014-05-16 11:06:58 -0500

    targets are tested and working working kinks out of outlets which are NOT working

--- src/main/java/com/heisenbugdev/heisenui/view/HeisenView.java
+++ src/main/java/com/heisenbugdev/heisenui/view/HeisenView.java
@@ -54,7 +54,7 @@ public class HeisenView
             HeisenView view = this.subviews().get(outlet.getKey());
             if (view != null)
             {
-                outlet.getValue().set(view);
+                outlet.set(view);
             }
         }
 
--- src/main/java/com/heisenbugdev/heisenui/view/UITarget.java
+++ src/main/java/com/heisenbugdev/heisenui/view/UITarget.java
@@ -1,8 +1,13 @@
 package com.heisenbugdev.heisenui.view;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 @Documented
+@Retention(RetentionPolicy.RUNTIME)
+@java.lang.annotation.Target(ElementType.METHOD)
 public @interface UITarget
 {
     String value();
